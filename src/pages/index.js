5c0f0f6099acce254d0aafa632b96ed95c348920
LivAlmeida6b <livia.araujo+6b@estudante.ifms.edu.br>
2024-09-30 15:45:13 +0000

    aula 30/09

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Footer from "./component/footer";
+import Navbar from "./component/navbar";
+
+function Index(){
+    return <>
+    <Navbar/>
+    <h1>Olá mundo</h1>;
+    <Footer/> 
+    </>
+   
+}
+export default Index;
