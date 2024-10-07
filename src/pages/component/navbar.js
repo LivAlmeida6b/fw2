76639e771fcfb058a177a0a059910f1cc9c9d53a
LivAlmeida6b <livia.araujo+6b@estudante.ifms.edu.br>
2024-10-07 15:42:45 +0000

    aula 7/10

--- src/pages/component/navbar.js
+++ src/pages/component/navbar.js
@@ -1,3 +1,19 @@
-export default function Navbar(){
-    return <div>Menu</div>
+import {Container, Nav, Navbar} from 'react-bootstrap';
+function Menu() {
+  return (
+    <Navbar expand="lg" className="bg-success p-2 text-white">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
+export default Menu;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import Footer from "./component/footer";
 import Navbar from "./component/navbar";
+import {Container, Row, Col} from 'react-bootstrap';
 
-function Index(){
+function Index() {
     return <>
-    <Navbar/>
-    <h1>Olá mundo</h1>;
-    <Footer/> 
-    </>
-   
+        <Navbar />
+        <Container>
+            <Row>
+                <Col className="bg-primary">1 of 2</Col>
+                <Col className="bg-primary-subtle">2 of 2</Col>
+            </Row>
+            <Row>
+                <Col className="bg-danger-subtle">1 of 3</Col>
+                <Col className="bg-warning">2 of 3</Col>
+                <Col className="bg-success">3 of 3</Col>
+            </Row>
+        </Container>
+        <Footer />
+    </>;
 }
 export default Index;
