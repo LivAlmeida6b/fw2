2af9765936e70dedc6e0ed75d83301fbf52d266a
LivAlmeida6b <livia.araujo+6b@estudante.ifms.edu.br>
2024-12-09 15:04:15 +0000

    aula 9/12

--- src/pages/component/navbar.js
+++ src/pages/component/navbar.js
@@ -1,9 +1,12 @@
-import {Container,Nav, Navbar} from 'react-bootstrap';
-import style from "@/styles/Navbar.module.css"
+import { Container, Nav, Navbar } from "react-bootstrap";
+import style from "@/styles/Navbar.module.css";
 export default function Menu() {
   return (
-  <Container>
-    <Navbar expand="sm" className={`${style.navGradiente} px-3 my-0 border-2 border border-success rounded rounded-3`}>
+    <Container>
+      <Navbar
+        expand="sm"
+        className={`${style.navGradiente} px-3 my-0 border-2 border border-success rounded rounded-3`}
+      >
         <Navbar.Brand href="/">6B</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -11,9 +14,12 @@ export default function Menu() {
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/noticias">Notícias</Nav.Link>
             <Nav.Link href="/promessa">Promessa</Nav.Link>
+            <Nav.Link href="/pages">Page</Nav.Link>
+            <Nav.Link href="/mesportes">P.Esportes</Nav.Link>
+            <Nav.Link href="/cardesportes">C.Esportes</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-    </Navbar>
- </Container>
+      </Navbar>
+    </Container>
   );
 }
